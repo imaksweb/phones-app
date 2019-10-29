fbeeba657ab49d8a942c601372342020fa944cfc
Illia Maksymov <ivashthestampede01@gmail.com>
2019-10-29 13:02:00 +0200

    Feature: Add cart component [ref: no-ref]

--- scripts/phone-details/phone-details.component.js
+++ scripts/phone-details/phone-details.component.js
@@ -1,15 +1,22 @@
 import BaseComponent from '../shared/components/base.component.js';
 
 class PhoneDetailsComponent extends BaseComponent {
-  constructor ({ element, onBack }) {
+  constructor ({ element, onBack, onAdd }) {
     super({ element });
     this._onBack = onBack;
+    this._onAdd = onAdd;
     this._element.addEventListener('click', (e) => {
       const backBtn = e.target.closest('.btn--back');
+      const addBtn = e.target.closest('.btn--add');
       const imgEl = e.target.closest('.thumb');
 
       if (backBtn) {
         this._onBack();
+        return;
+      }
+
+      if (addBtn) {
+        this._onAdd(this._phone.id);
         return;
       }
 
@@ -34,7 +41,7 @@ class PhoneDetailsComponent extends BaseComponent {
     <img class="phone">
 
     <button class="btn--back">Back</button>
-    <button>Add to basket</button>
+    <button class="btn--add">Add to basket</button>
 
 
     <h1>${this._phone.name}</h1>
